4581f891a8a2dfb3bdbc664ab89c86a818df0da6
mridul-netizen <mridulsetia18@gmail.com>
2021-01-19 12:16:17 +0530

    Optimizations

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://Mridul:Mridul@18@cluster0.v7lqf.mongodb.net/Database?retryWrites=true&w=majority", { useNewUrlParser: true , useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://Mridul:"+process.env.MONGO_ATLAS_PW+"@cluster0.v7lqf.mongodb.net/Database?retryWrites=true&w=majority", { useNewUrlParser: true , useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to database!");
   })
